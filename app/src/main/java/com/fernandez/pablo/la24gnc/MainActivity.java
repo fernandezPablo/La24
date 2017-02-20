d0ec4bdd3a6f56ef0a83c96d36ee8b4c04f77919
pablo fernandez <pablo fernandez>
2017-02-19 21:55:21 -0300

    commit inicial

---
+++ app/src/main/java/com/fernandez/pablo/la24gnc/MainActivity.java
@@ -0,0 +1,27 @@
+package com.fernandez.pablo.la24gnc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickAbrirTurno(View v){
+        Toast.makeText(this, "ABRIENDO TURNO...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickVenta(View v){
+        Toast.makeText(this, "INICIANDO VENTA...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickCerrarTurno(View v){
+        Toast.makeText(this, "CERRANDO TURNO...", Toast.LENGTH_SHORT).show();
+    }
+}
