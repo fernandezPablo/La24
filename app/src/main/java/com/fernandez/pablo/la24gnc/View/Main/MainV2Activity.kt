e9701e0f9d5ca7161fa3f21529aba39b8e38246f
PABLO <fernandez.pablo44@gmail.com>
2017-06-24 14:15:22 -0300

    Agregados iconos a la vista principal

--- app/src/main/java/com/fernandez/pablo/la24gnc/View/Main/MainV2Activity.kt
+++ app/src/main/java/com/fernandez/pablo/la24gnc/View/Main/MainV2Activity.kt
@@ -3,6 +3,9 @@ package com.fernandez.pablo.la24gnc.View.Main
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ImageSpan
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
