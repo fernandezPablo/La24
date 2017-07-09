11f8c21539006aad1daa90c37b40c862ba10cc4f
PABLO <fernandez.pablo44@gmail.com>
2017-07-08 23:24:03 -0300

    Agregado activity abmPRoductos

---
+++ app/src/main/java/com/fernandez/pablo/la24gnc/View/ABMProductos/ABMProductosActivity.kt
@@ -0,0 +1,14 @@
+package com.fernandez.pablo.la24gnc.View.ABMProductos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.fernandez.pablo.la24gnc.R
+
+class ABMProductosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abmproductos)
+    }
+}
