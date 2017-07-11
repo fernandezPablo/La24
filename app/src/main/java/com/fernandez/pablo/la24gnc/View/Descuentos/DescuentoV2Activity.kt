ad1aec9452e9bca172c186d1893ce89853e619a8
PABLO <fernandez.pablo44@gmail.com>
2017-07-10 23:04:26 -0300

    Creadas las vistas para la segunda version de los descuentos (buzon/vale)

---
+++ app/src/main/java/com/fernandez/pablo/la24gnc/View/Descuentos/DescuentoV2Activity.kt
@@ -0,0 +1,15 @@
+package com.fernandez.pablo.la24gnc.View.Descuentos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.fernandez.pablo.la24gnc.R
+
+class DescuentoV2Activity:AppCompatActivity() {
+
+    protected override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_descuento_v2)
+    }
+
+}
