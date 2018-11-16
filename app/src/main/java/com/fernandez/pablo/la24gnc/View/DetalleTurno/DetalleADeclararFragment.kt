e3a2858e69393a84fa5f4588bd629d4dfe191603
PABLO <fernandez.pablo44@gmail.com>
2018-11-16 00:20:34 -0300

    Generate "Vale" from "Buzon" in the DetalleTurnoView

--- app/src/main/java/com/fernandez/pablo/la24gnc/View/DetalleTurno/DetalleADeclararFragment.kt
+++ app/src/main/java/com/fernandez/pablo/la24gnc/View/DetalleTurno/DetalleADeclararFragment.kt
@@ -12,9 +12,7 @@ import android.widget.*
 
 import com.fernandez.pablo.la24gnc.Model.Descuento
 import com.fernandez.pablo.la24gnc.Model.DescuentoDAO
-import com.fernandez.pablo.la24gnc.Model.Venta
 import com.fernandez.pablo.la24gnc.R
-import com.fernandez.pablo.la24gnc.Service.DbHelper
 import com.fernandez.pablo.la24gnc.View.Utils.DescuentoAdapter
 
 import java.util.ArrayList
@@ -30,11 +28,17 @@ class DetalleADeclararFragment : Fragment() {
     private var descuentoAdapter: DescuentoAdapter? = null
     private var descuentoSeleccionado: Descuento? = null
     private var posicionDescuentoSeleccionado: Int? = null
-    private var alertDialog: AlertDialog? = null
-    private var alertDialogView: View? = null
-    private var montoEditText: EditText? = null
-    private var cancelarButton: Button? = null
+    private var alertDialogDivBuzon: AlertDialog? = null
+    private var alertDialogViewDivBuzon: View? = null
+    private var montoBuzonEditText: EditText? = null
+    private var cancelarDivBuzonButton: Button? = null
     private var dividirButton: Button? = null
+    private var alertDialogGenVale: AlertDialog? = null
+    private var alertDialogViewGenVale: View? = null
+    private var descripcionValeEditText: EditText? = null
+    private var montoValeEditText: EditText? = null
+    private var cancelarGenValeButton: Button? = null
+    private var genValeButton: Button? = null
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
@@ -52,24 +56,52 @@ class DetalleADeclararFragment : Fragment() {
             parent, view, position, id -> this.onItemLongClick(position)
         }
 
-        val mBuilder = AlertDialog.Builder(activity)
+        /*
+        * GENERATE DIALOG VIEW FOR DIVIDIR BUZON
+        * */
+        val mBuilderDivBuzon = AlertDialog.Builder(activity)
 
-        this.alertDialogView = inflater.inflate(R.layout.alert_dialog_dividir_descuento,container,false)
-        this.montoEditText = this!!.alertDialogView!!.findViewById(R.id.etMonto) as EditText
-        this.cancelarButton = this!!.alertDialogView!!.findViewById(R.id.btnCancelar) as Button
-        this.cancelarButton!!.setOnClickListener {
-            this.alertDialog!!.dismiss()
+        this.alertDialogViewDivBuzon = inflater.inflate(R.layout.alert_dialog_dividir_descuento,container,false)
+        this.montoBuzonEditText = this!!.alertDialogViewDivBuzon!!.findViewById(R.id.etMonto) as EditText
+        this.cancelarDivBuzonButton = this!!.alertDialogViewDivBuzon!!.findViewById(R.id.btnCancelar) as Button
+        this.cancelarDivBuzonButton!!.setOnClickListener {
+            this.alertDialogDivBuzon!!.dismiss()
         }
-        this.dividirButton = this!!.alertDialogView!!.findViewById(R.id.btnDividir) as Button
+        this.dividirButton = this!!.alertDialogViewDivBuzon!!.findViewById(R.id.btnDividir) as Button
         this.dividirButton!!.setOnClickListener{
             this.dividirBuzon(it)
-            this.alertDialog!!.dismiss()
-            this.montoEditText!!.text.clear()
+            this.alertDialogDivBuzon!!.dismiss()
+            this.montoBuzonEditText!!.text.clear()
         }
 
-        mBuilder.setView(this.alertDialogView)
+        mBuilderDivBuzon.setView(this.alertDialogViewDivBuzon)
 
-        this.alertDialog = mBuilder.create()
+        this.alertDialogDivBuzon = mBuilderDivBuzon.create()
+
+        /*
+        * GENERATE DIALOG VIEW FOR GENERAR VALE
+        * */
+
+        val mBuilderGenVale = AlertDialog.Builder(activity)
+
+        this.alertDialogViewGenVale = inflater.inflate(R.layout.alert_dialog_generar_vale,container,false)
+        this.descripcionValeEditText = this!!.alertDialogViewGenVale!!.findViewById(R.id.etDescripcionVale) as EditText
+        this.montoValeEditText = this!!.alertDialogViewGenVale!!.findViewById(R.id.etMonto) as EditText
+        this.cancelarGenValeButton = this!!.alertDialogViewGenVale!!.findViewById(R.id.btnCancelar) as Button
+        this.cancelarGenValeButton!!.setOnClickListener {
+            this.alertDialogGenVale!!.dismiss()
+        }
+        this.genValeButton = this!!.alertDialogViewGenVale!!.findViewById(R.id.btnGenerarVale) as Button
+        this.genValeButton!!.setOnClickListener{
+            this.generarVale(it)
+            this.alertDialogGenVale!!.dismiss()
+            this.descripcionValeEditText!!.text.clear()
+            this.montoValeEditText!!.text.clear()
+        }
+
+        mBuilderGenVale.setView(this.alertDialogViewGenVale)
+
+        this.alertDialogGenVale = mBuilderGenVale.create()
 
         (activity as DetalleTurnoActivity).cargarDetalleADeclarar()
 
@@ -77,7 +109,7 @@ class DetalleADeclararFragment : Fragment() {
     }
 
     private fun dividirBuzon(it: View?) {
-        val montoADividir: Double = this.montoEditText!!.text.toString().toDouble()
+        val montoADividir: Double = this.montoBuzonEditText!!.text.toString().toDouble()
         if(montoADividir < this.descuentoSeleccionado!!.monto){
             val descuentoNuevo = Descuento("Buzon",
                     Descuento.BUZON,montoADividir, this.descuentoSeleccionado!!.venta!!)
@@ -93,6 +125,23 @@ class DetalleADeclararFragment : Fragment() {
         }
     }
 
+    private fun generarVale(it: View){
+        val montoVale = this.montoValeEditText!!.text.toString().toDouble()
+        if(montoVale <= this.descuentoSeleccionado!!.monto){
+            val descuentoNuevo = Descuento(this.descripcionValeEditText!!.text.toString(),
+                    Descuento.VALE,montoVale,this.descuentoSeleccionado!!.venta!!)
+            this.descuentos!!.add(descuentoNuevo)
+            DescuentoDAO(activity).createDescuento(descuentoNuevo)
+            this.descuentoSeleccionado!!.monto -= montoVale
+            this.descuentos!!.set(this.posicionDescuentoSeleccionado!!,this.descuentoSeleccionado!!)
+            DescuentoDAO(activity).updateDescuento(this.descuentoSeleccionado!!)
+            this.descuentoAdapter!!.notifyDataSetChanged()
+        }
+        else{
+            Toast.makeText(activity,"El monto ingresado es mayor al monto actual del buzon",Toast.LENGTH_SHORT).show()
+        }
+    }
+
     fun cargarListaDescuentos(descuentos: ArrayList<Descuento>) {
         for (des in descuentos) {
             this.descuentos!!.add(des)
@@ -105,7 +154,17 @@ class DetalleADeclararFragment : Fragment() {
             this.descuentoSeleccionado = this.descuentos!![position]
             this.posicionDescuentoSeleccionado = position
             Toast.makeText(activity,"Buzon: ${this.descuentoSeleccionado}",Toast.LENGTH_LONG).show()
-            alertDialog!!.show()
+            val optionsDialog : AlertDialog.Builder = AlertDialog.Builder(activity)
+            optionsDialog.setTitle("Seleccione una opcion")
+            optionsDialog.setItems(arrayOf("Dividir Buzon","Generar Vale"), DialogInterface.OnClickListener { dialogInterface, i ->
+                when(i){
+                    0 -> alertDialogDivBuzon!!.show()
+                    1 -> alertDialogGenVale!!.show()
+                    else -> Toast.makeText(activity,"La opcion seleccionada no fue definida",Toast.LENGTH_SHORT).show()
+                }
+            })
+            optionsDialog.show()
+            //alertDialogDivBuzon!!.show()
         }
         else{
             Toast.makeText(activity,"Los vales no se pueden dividir",Toast.LENGTH_SHORT).show()
